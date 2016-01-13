476f04c187c0456e72f3fa3248fafc2627457a74
ejspeiro <ejspeiro@gmail.com>
2016-01-13 09:48:14 +0100

    Update README.md. Update .gitignore. Create example about pipelining.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # HPC-Practices
-Practicing concepts in HPC: From sequential to parallel computing
+Practicing concepts in HPC: From sequential to parallel computing.
+
+This repository contains examples from this book about High-Performance
+Scientific Computing:
+
+http://pages.tacc.utexas.edu/~eijkhout/Articles/EijkhoutIntroToHPC.pdf
+
+# Author
+
+Eduardo J. Sanchez, PhD.
+
---
+++ pipelining.c
@@ -0,0 +1,91 @@
+// Main reference: 1. http://pages.tacc.utexas.edu/~eijkhout/istc/istc.html
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+  clock_t begin_time;
+
+  // 2 arrays of 3.2 GB of size each (for a total of 6.4 GB) fit in the RAM.
+  size_t nn = 400000000;
+  size_t total_mem_array = nn*sizeof(double);
+
+  double *aa;
+  double *aa_free;
+  double *bb;
+  double *bb_free;
+  double ss = 0.0;
+  double time_test = 0.0;
+
+  aa = (double *) malloc(total_mem_array);
+  aa_free = aa;
+  bb = (double *) malloc(total_mem_array);
+  bb_free = bb;
+
+  int ii = 0;
+
+  for (ii = 0; ii < nn; ++ii) {
+    aa[ii] = 1.0;
+    bb[ii] = 1.0;
+  }
+
+  // Pipelines require a number of independent operations to function
+  // efficiently. An example of an operation that can not be pipelined is the
+  // inner product sum ss [1]:
+
+  begin_time = clock();
+  for (ii = 0; ii < nn; ++ii) {
+    ss += aa[ii]*bb[ii];
+  }
+  time_test = ((double) clock() - begin_time)/CLOCKS_PER_SEC;
+
+  fprintf(stdout, "Memory arrays: %zu B.\n", 2*total_mem_array);
+
+  fprintf(stdout, "Time test 1: %f s.\n", time_test);
+  fprintf(stdout, "ss = %f\n", ss);
+
+  // The fact that ss gets both read and written halts the addition pipeline.
+  // One way to fill the floating point pipeline is to apply LOOP UNROLLING [1]:
+
+  ss = 0.0;
+  double sum1 = 0.0;
+  double sum2 = 0.0;
+
+  begin_time = clock();
+  for (ii = 0; ii < nn/2 - 1; ++ii) {
+    sum1 += aa[2*ii]*bb[2*ii];
+    sum2 += aa[2*ii + 1]*bb[2*ii + 1];
+  }
+  ss = sum1 + sum2;
+  time_test = ((double) clock() - begin_time)/CLOCKS_PER_SEC;
+
+  fprintf(stdout, "Time test 2: %f s.\n", time_test);
+  fprintf(stdout, "ss = %f\n", ss);
+
+  // Note that there are two independent multiplies in between the
+  // accumulations. With a little INDEXING OPTIMIZATION this becomes [1]:
+
+  ss = 0.0;
+  sum1 = 0.0;
+  sum2 = 0.0;
+
+  begin_time = clock();
+  for (ii = 0; ii < nn/2 - 1; ++ii) {
+    sum1 += (*(aa + 0))*(*(bb + 0));
+    sum2 += (*(aa + 1))*(*(bb + 1));
+    aa += 2;
+    bb += 2;
+  }
+  ss = sum1 + sum2;
+  time_test = ((double) clock() - begin_time)/CLOCKS_PER_SEC;
+
+  fprintf(stdout, "Time test 3: %f s.\n", time_test);
+  fprintf(stdout, "ss = %f\n", ss);
+
+  free(aa_free);
+  free(bb_free);
+
+  return 0;
+}
